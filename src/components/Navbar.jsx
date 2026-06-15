3efbac30900d418ab82506494a952d1e6c1e3526
Shashikanth <shashikanthdussa08@gmail.com>
2026-06-15 18:16:17 +0530

    bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,83 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { removeUser } from "../utils/userSlice";
 import { BASEURL } from "../constants";
 import axios from "axios";
-const Navbar = () =>{
-    const user = useSelector(store=>store.user)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const logoutHandler = async() =>{
-     try{
-         await axios.post(BASEURL + "/logout",{}, {
-        withCredentials: true,
-      });
-    dispatch(removeUser())
-    navigate("/login")
-     }catch(err){
-      console.log(err)
-     }
+const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logoutHandler = async () => {
+    try {
+      await axios.post(
+        BASEURL + "/logout",
+        {},
+        {
+          withCredentials: true,
+        },
+      );
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
     }
-    return(
-         <div className="navbar bg-base-200 shadow-sm">
-  <div className="flex-1">
-    <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
-  </div>
-  <div className="flex gap-2">
-    {user && 
-    <>
-    <p>Hello {user.firstName}</p>
-    <div className="dropdown dropdown-end mx-5">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
+  };
+  return (
+    <div className="navbar bg-base-200 shadow-sm">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">
+          DevTinder
+        </Link>
       </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <Link  to="/profile" className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </Link>
-        </li>
-        <li>
-          <Link  to="/connections" className="justify-between">
-            Connections
-            <span className="badge">New</span>
-          </Link>
-        </li>
-        <li>
-          <Link  to="/requests" className="justify-between">
-            Request
-          </Link>
-        </li>
-        <li><a>Settings</a></li>
-        <button onClick={logoutHandler}>logout</button>
-      </ul>
+      <div className="flex gap-2">
+        {user && (
+          <>
+            <p>Hello {user.firstName}</p>
+            <div className="dropdown dropdown-end mx-5">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <Link to="/profile" className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/connections" className="justify-between">
+                    Connections
+                    <span className="badge">New</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/requests" className="justify-between">
+                    Request
+                  </Link>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <button onClick={logoutHandler}>logout</button>
+              </ul>
+            </div>
+          </>
+        )}
+      </div>
     </div>
-    </>
-    }
-  </div>
-</div>
-    )
-}
-export default Navbar
+  );
+};
+export default Navbar;
